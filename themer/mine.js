658b8b6fbe48a40e57786fdb2d93993b22508ffd
Kasama <robertoaall@gmail.com>
2021-06-04 15:41:51 -0300

    Update submodules

---
+++ themer/mine.js
@@ -0,0 +1,49 @@
+exports.colors = {
+
+  // A color set can have both light and dark variants, but is only required
+  // to have one.
+  dark: {
+
+    // Colors can be defined in any valid CSS format.
+
+    // accent0-7 should be the main accent colors of your theme. See the table
+    // in the "Color mappings" section for how the colors will be used in your
+    // new themes.
+    accent0: '#FF4050',
+    accent1: '#F28144',
+    accent2: '#FFD24A',
+    accent3: '#A4CC35',
+    accent4: '#26C99E',
+    accent5: '#66BFFF',
+    accent6: '#CC78FA',
+    accent7: '#F553BF',
+
+    // shade0-7 should be shades of the same hue, with shade0 being the
+    // background and shade7 being the foreground. If you omit the
+    // intermediate shades (1 through 6), they will be calculated automatically
+    // for you.
+    shade0: '#282629',
+    shade1: '#474247',
+    shade2: '#656066',
+    shade3: '#847E85',
+    shade4: '#A29DA3',
+    shade5: '#C1BCC2',
+    shade6: '#E0DCE0',
+    shade7: '#FFFCFF'
+
+  },
+
+  // Same as above, except that shade0 should be the lightest and shade7 should
+  // be the darkest.
+  light: {
+    shade0: '#FFFCFF'
+    shade1: '#E0DCE0',
+    shade2: '#C1BCC2',
+    shade3: '#A29DA3',
+    shade4: '#847E85',
+    shade5: '#656066',
+    shade6: '#474247',
+    shade7: '#282629',
+  },
+
+};
